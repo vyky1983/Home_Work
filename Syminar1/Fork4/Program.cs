2f9eee82e9f85b9fda166663724e342e43dd2b0c
Veronica <veronica.cernenchii@mail.ru>
2022-07-20 19:11:45 +0300

    Fork4

--- Syminar1/Fork4/Program.cs
+++ Syminar1/Fork4/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+int num;
+Console.WriteLine("Enter your number:");
+ num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < num ; i ++)
+
+{
+    if(i % 2 == 0)
+
+    Console.WriteLine("You number: {0} ", i);
+}
