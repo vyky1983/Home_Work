aee8a0af0b2e59d84b7fe3fc33e3e1e3a8afce30
Veronica <veronica.cernenchii@mail.ru>
2022-07-20 18:52:03 +0300

    исправила код

--- Syminar1/Fork2/Program.cs
+++ Syminar1/Fork2/Program.cs
@@ -4,14 +4,20 @@
 44 5 78 -> 78
 22 3 9 -> 22*/
 
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
 int num, num1, num2, res;
-Console.WriteLine("Enter number:");
+Console.WriteLine("Enter 3 number:");
 num = Convert.ToInt32(Console.ReadLine());
 num1 = Convert.ToInt32(Console.ReadLine());
 num2 = Convert.ToInt32(Console.ReadLine());
 res = Math.Max(num, num1);
 
-Console.WriteLine("Ваше максимальное число: {0} ", res);
+//Console.WriteLine("Ваше максимальное число: {0} ", res);
 if(res > num2)
 {
     Console.WriteLine("Max: {0} " , res);
