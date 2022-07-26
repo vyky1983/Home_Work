8c5c1dc16efadfafb62ffaafebcf31a839027411
Veronica <veronica.cernenchii@mail.ru>
2022-07-26 16:25:47 +0300

    HomeWork 3

--- Syminar2/Fork3/Program.cs
+++ Syminar2/Fork3/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//System.Console.WriteLine("Задача 3 : Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+System.Console.WriteLine("Enter numbre day -> ");
+int number_day = Convert.ToInt32(Console.ReadLine());
+
+if (number_day == 6 || number_day == 7)
+{
+    System.Console.WriteLine("This number  correspond to the day of the week, weekend ");
+}
+else if (number_day < 1 || number_day > 7)
+{
+    System.Console.WriteLine("This number does not correspond to the day of the week");
+}
+else
+{
+    System.Console.WriteLine("This number  correspond to the day of the week, working day");
+}
