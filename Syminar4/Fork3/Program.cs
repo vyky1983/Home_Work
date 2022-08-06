eadd00ed345434b6da2030dce53ad9aa60d7ac8d
Veronica <veronica.cernenchii@mail.ru>
2022-08-06 17:57:17 +0300

    Fork 3

---
+++ Syminar4/Fork3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] array = new int[8];
+Random random = new Random();
+System.Console.Write("[");
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 99);
+    System.Console.Write(array[i] + ",");
+}
+
+System.Console.Write("]");
