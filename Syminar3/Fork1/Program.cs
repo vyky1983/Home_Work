065e0a237f9dee279bb37c1ab07b1f1311b0555a
Veronica <veronica.cernenchii@mail.ru>
2022-09-29 09:08:33 +0300

    Modofic

--- Syminar3/Fork1/Program.cs
+++ Syminar3/Fork1/Program.cs
@@ -1,4 +1,4 @@
-﻿/*Задача 19
+﻿﻿/*Задача 19
 
 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
@@ -8,19 +8,24 @@
 
 23432 -> да*/
 
+using static System.Console;
+Clear();
+
 Console.WriteLine("Введите пятизначное число -> ");
 string? num = Console.ReadLine();
-
-if (num?[0] == num?[4] || num?[1] == num?[3])
-{
-    System.Console.WriteLine("Ваше число полидром -> " + num);
-}
-else
-{
-    System.Console.WriteLine("Ваше число не полидром -> " + num);
-}
-
 if (num?.Length > 5)
 {
     System.Console.WriteLine("Введите правельно число, ваше число не пятизначное");
+    return;
 }
+
+if (num?[0] == num?[4] || num?[1] == num?[3])
+{
+    System.Console.WriteLine($"Ваше число полидром -> {num}");
+}
+else
+{
+    System.Console.WriteLine($"Ваше число не полидром -> {num}");
+}
+
+
