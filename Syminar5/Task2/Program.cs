2cc4decac8d4465705b2f6cf5931ce215f53fd0b
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-14 18:34:48 +0300

    HomeWork Syminar5

---
+++ Syminar5/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+Clear();
+
+/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+
+WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
+
+    WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+   Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+           Write(numbers[i] + " ");
+        }
+    Write("]");
+    WriteLine();
+}
